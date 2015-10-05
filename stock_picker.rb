9332f64510b57d8d1a712ac4eaf11231aca085ae
Eric M <ericmarchese77@gmail.com>
2015-10-05 19:21:52 -0400

    Completed method

---
+++ stock_picker.rb
@@ -0,0 +1,24 @@
+# Takes in an array of stock prices, one for each hypothetical day. It will 
+# return a pair of days representing the best day to buy and the best day to 
+# sell. Days start at 0.
+
+def stock_picker(array)
+  low_index = 0
+  high_index = 0
+  greatest_difference = 0
+
+  array.each do |price1|
+    array.each do |price2|
+      if ((price2 - price1) > greatest_difference) && (array.index(price1) <
+        array.index(price2))
+        greatest_difference = price2 - price1
+        low_index = array.index(price1)
+        high_index = array.index(price2)
+      end
+    end
+  end
+  [low_index, high_index]
+end
+
+puts "Buy: #{stock_picker([17,3,6,9,15,8,6,1,10])[0]}"
+puts "Sell: #{stock_picker([17,3,6,9,15,8,6,1,10])[1]}"
